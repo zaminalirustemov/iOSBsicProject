94ef1e508f045d62b518721ecb8f1fae11924ade
Zaminali <zaminali.rustamov@azintelecom.az>
2025-02-17 11:43:55 +0400

    Initial Commit

---
+++ FirstXCodeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstXCodeProject
+//
+//  Created by Zamin on 17.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
