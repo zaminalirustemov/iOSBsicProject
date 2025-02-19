d0603b115295bdff0f70d5f8a4cec104eaba1c56
Zaminali <zaminali.rustamov@azintelecom.az>
2025-02-19 11:02:43 +0400

    initial commit

--- FirstXCodeProject/ViewController.swift
+++ FirstXCodeProject/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var imageView2: UIImageView!
+    
+    var hs: UIView = UIView()
+    
+    var ghL:UIView = {
+       var s = UIView()
+        s.backgroundColor = .red
+        return s
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func buttonClick(_ sender: Any) {
+        imageView2.image = UIImage(named: "2ndPhoto")
+        
+    }
+    
 }
-
